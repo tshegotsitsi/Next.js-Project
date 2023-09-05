30b6c9b4ad66ab39f28ec740640be877bfd68055
tshegotsitsi <125869406+tshegotsitsi@users.noreply.github.com>
2023-09-05 16:12:20 +0200

    Navigating to the "Filtered Events" Page Progammatically

--- section-4/components/event/events-search.js
+++ section-4/components/event/events-search.js
@@ -1,38 +1,53 @@
+import { useRef } from "react";
+
 import Button from "./ui/button";
-import classes from '/events-search.module.css';
+import classes from "./event-search.module.css";
 
 function EventsSearch(props) {
 
-    return (
-        <form className={classes.form}>
-            <div className={classes.controls}>
-                <div className={classes.control}>
-                    <label htmlFor="year">Year</label>
-                    <select id='year'>
-                        <option value="2021">2021</option>
-                        <option value="2022">2022</option>
-                    </select>
-                </div>
-                <div className={classes.control}>
-                    <label htmlFor="month">Month</label>
-                        <select>
-                            <option value='1'>Jan</option>
-                            <option value='2'>Feb</option>
-                            <option value='3'>Mar</option>
-                            <option value='4'>Apr</option>
-                            <option value='5'>May</option>
-                            <option value='6'>Jun</option>
-                            <option value='7'>Jul</option>
-                            <option value='8'>Aug</option>
-                            <option value='9'>Sep</option>
-                            <option value='10'>Oct</option>
-                            <option value='11'>Nov</option>
-                            <option value='12'>Dec</option>
-                        </select>
-                  
-                </div>
-            </div>
-            <Button>Find </Button>
-        </form>
-    )
+    const yearInputRef = useRef();
+    const monthInputRef = useRef();
+
+ function submitHandler(event) {
+    event.preventDefault();
+
+    const selectedYear = yearInputRef.current.value
+    const selectedMonth = monthInputRef.current.value
+ }
+
+        props.onSearch(selectedYear, selectedMonth)
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <div className={classes.controls}>
+        <div className={classes.control}>
+          <label htmlFor="year">Year</label>
+          <select id="year" ref={yearInputRef}>
+            <option value="2021">2021</option>
+            <option value="2022">2022</option>
+          </select>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="month">Month</label>
+          <select id="month" ref={monthInputRef}>
+            <option value="1">January</option>
+            <option value="2">February</option>
+            <option value="3">March</option>
+            <option value="4">April</option>
+            <option value="5">May</option>
+            <option value="6">June</option>
+            <option value="7">July</option>
+            <option value="8">August</option>
+            <option value="9">September</option>
+            <option value="10">October</option>
+            <option value="11">November</option>
+            <option value="12">December</option>
+          </select>
+        </div>
+      </div>
+      <Button>Find Events</Button>
+    </form>
+  );
 }
+
+export default EventsSearch;
