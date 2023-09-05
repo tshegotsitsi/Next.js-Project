38f6f1e253e9a6e73e5be4d27f64735a53a0f49d
tshegotsitsi <125869406+tshegotsitsi@users.noreply.github.com>
2023-09-05 15:03:29 +0200

    Adding a Filter Form for Filtering Events

--- section-4/pages/events/index.js
+++ section-4/pages/events/index.js
@@ -1,13 +1,16 @@
+import { Fragment } from "react";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/event/event-list";
+import EventsSearch from "../../components/events/events-search"; // Ensure correct path
 
-function AllEventsPage(){
-    const events = getAllEvents();
-    return (
-        <div>   
-            <EventList items={events}/>
-        </div>
-    )
+function AllEventsPage() {
+  const events = getAllEvents();
+  return (
+    <Fragment>
+      <EventsSearch />
+      <EventList items={events} />
+    </Fragment>
+  );
 }
 
 export default AllEventsPage;
