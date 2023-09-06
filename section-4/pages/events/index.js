ff2ab5375157ad69de0305cec497d5f46d8cfaf6
tshegotsitsi <125869406+tshegotsitsi@users.noreply.github.com>
2023-09-06 09:58:12 +0200

    Add results components

--- section-4/pages/events/index.js
+++ section-4/pages/events/index.js
@@ -1,16 +1,30 @@
-import { Fragment } from "react";
 import { getAllEvents } from "../../dummy-data";
-import EventList from "../../components/event/event-list";
-import EventsSearch from "../../components/events/events-search"; // Ensure correct path
+import EventList from '../../components/event/event-list';
+import EventsSearch from "../../components/event/events-search";
+import { Fragment } from "react";
+import { useRouter } from 'next/router';
 
-function AllEventsPage() {
+function AllEventsPage(){
+  const router=useRouter();
   const events = getAllEvents();
-  return (
-    <Fragment>
-      <EventsSearch />
-      <EventList items={events} />
-    </Fragment>
-  );
+ 
+
+  function findEventsHandler(year, month){
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+   
+  }
+
+    return (
+
+        <Fragment>
+             <EventsSearch onSearch={findEventsHandler}/>  
+            <EventList items={events} />
+        </Fragment>
+           
+       
+    )
 }
 
 export default AllEventsPage;
